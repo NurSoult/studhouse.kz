c3b63dc97abb2ff7fe6e9b2cea4e9b1a32a696b7
nursen <200103098@stu.sdu.edu.kz>
2024-02-21 22:01:58 +0600

    Added

---
+++ src/main/java/sdu/diploma/studhouse/StudhouseApplication.java
@@ -0,0 +1,14 @@
+package sdu.diploma.studhouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudhouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudhouseApplication.class, args);
+		System.out.println("HERE WE GO!");
+	}
+
+}
---
+++ src/test/java/sdu/diploma/studhouse/StudhouseApplicationTests.java
@@ -0,0 +1,13 @@
+package sdu.diploma.studhouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudhouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
